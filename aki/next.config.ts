fb5f337dfbad9416984281d6e000f383dfaecaaf
lebenmitparanoia-tech <leben.mit.paranoia@web.de>
2025-09-03 18:46:31 +0200

    Delete aki directory

--- aki/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: false },
-}
-
-export default nextConfig
